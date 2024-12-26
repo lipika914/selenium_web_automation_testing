f8379befbe6b68c7f6323f341eac36849a18afd3
lipika914 <54241426+lipika914@users.noreply.github.com>
2024-12-26 20:48:47 +0800

    Update base_page.py

--- utils/base_page.py
+++ utils/base_page.py
@@ -1,17 +1,3 @@
-# from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.webdriver.support import expected_conditions as EC
-#
-#
-# class BasePage:
-#     def __init__(self, driver):
-#         self.driver = driver
-#
-#     def wait_for_element(self, by, value, timeout=100):
-#         return WebDriverWait(self.driver, timeout).until(
-#             EC.presence_of_element_located((by, value))
-#         )
-#     def quit(self):
-#         self.driver.quit()
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
